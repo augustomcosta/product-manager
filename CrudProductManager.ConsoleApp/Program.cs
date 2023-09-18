bb39d809b4a6e65df0bb9dd08f94edc7ab1fe1b4
Augusto <augusto.medeirosdacosta@gmail.com>
2023-09-17 23:46:32 -0300

    feat: add exception handling to ConsoleApp

---
+++ CrudProductManager.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using CrudProductManager.API.Domain.Models;
+using CrudProductManager.ConsoleApp.Services;
+
+Console.WriteLine("## Product Manager ##\n");
+
+ApiService apiService = new();
+
+while (true)
+{
+    Console.WriteLine("1. Add Product");
+    Console.WriteLine("2. Remove Product by Id");
+    Console.WriteLine("3. Edit product by Id");
+    Console.WriteLine("4. Get all products");
+    Console.WriteLine("5. Exit");
+
+    string? userInputString = (Console.ReadLine());
+
+    if (int.TryParse(userInputString, out int userInput))
+    {
+        switch (userInput)
+        {
+            case 1:
+                Console.WriteLine("\nType the name of the product:");
+                string? productName = Console.ReadLine();
+                Console.WriteLine("\nType the description of the product:");
+                string? productDescription = Console.ReadLine();
+                Console.WriteLine("\nType the price of the product:");
+                decimal productPrice = Convert.ToDecimal(Console.ReadLine());
+
+                Product newProduct = new Product(0, productName!, productDescription!, productPrice);
+
+                HttpResponseMessage responseCreate = await apiService.Create(newProduct);
+
+                if (responseCreate.IsSuccessStatusCode)
+                {
+                    string responseContent = await responseCreate.Content.ReadAsStringAsync();
+                    
+                    int newProductId;
+                    
+                    if (int.TryParse(responseContent, out newProductId))
+                    {
+                        Console.WriteLine($"Product added sucessfully. ID: {newProductId}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error adding product. Status {responseCreate.StatusCode} ");
+                }
+
+                break;
+        }
+    }
+}
---
+++ CrudProductManager.ConsoleApp/Services/ApiService.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using CrudProductManager.API.Domain.Models;
+
+namespace CrudProductManager.ConsoleApp.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public ApiService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:5268/");
+        }
+
+        public async Task<HttpResponseMessage> GetAll()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("api/Product");
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Successfully found products: {response}");
+                }
+                else
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Failed to get products. Status Code: {response.StatusCode}, Content: {content}");
+                }
+
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"General Exception: {e}");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponseMessage> GetById(int id)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"api/Product/{id}");
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Successfully found the product: {response}");
+                }
+                else
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Failed to get product. Status Code: {response.StatusCode}, Content: {content}");
+                }
+
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"General Exception: {e}");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponseMessage> Create(Product newProduct)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("api/Product", newProduct);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Successfully created the product");
+                }
+                else
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Failed to create the product. Status Code: {response.StatusCode}, Content: {content}");
+                }
+
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"General Exception: {e}");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponseMessage> Update(int id, Product updatedProduct)
+        {
+            try
+            {
+                var response = await _httpClient.PutAsJsonAsync($"api/Product/{id}", updatedProduct);
+                string content = await response.Content.ReadAsStringAsync();
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Successfully updated the product: {content}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to update the product. Status Code: {response.StatusCode}, Content: {content}");
+                }
+
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"General Exception: {e}");
+                throw;
+            }
+        }
+
+        public async Task<HttpResponseMessage> Delete(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"api/Product/{id}");
+                string content = await response.Content.ReadAsStringAsync();
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Successfully deleted the product: {content}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to delete the product. Status Code: {response.StatusCode}, Content: {content}");
+                }
+
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"HttpRequestException: {e}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"General Exception: {e}");
+                throw;
+            }
+        }
+    }
+}
